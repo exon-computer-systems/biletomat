95c4546f719365922e376c4ce6f2ee4eb07d65cf
Szymon <admin@example.com>
2024-07-17 15:38:51 +0200

    Add icons to navbar

--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -1,26 +1,38 @@
 import React from "react";
+import { faHeart, faUser } from "@fortawesome/free-solid-svg-icons";
 import "./Navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Navbar = () => {
-  return (
-    <header className="navbar">
-      <div className="logo">
-        <img
-          className="logoImage"
-          src="https://motocms.exon.pl/mt-content/uploads/2021/08/exon-logo-biel.png"
-          alt="exon logo"
-        />
-      </div>
-      <div className="icons">
-        <div className="favorites">
-          <button type="button">favorites</button>
-        </div>
-        <div className="login">
-          <button type="button">login</button>
-        </div>
-      </div>
-    </header>
-  );
+    return (
+        <header className="navbar">
+            <div className="logo">
+                <img
+                    className="logoImage"
+                    src="https://motocms.exon.pl/mt-content/uploads/2021/08/exon-logo-biel.png"
+                    alt="exon logo"
+                />
+            </div>
+            <div className="icons">
+                <div className="favorites">
+                    <button
+                        type="button"
+                        onClick={() => console.log("Favourite clicked")}
+                    >
+                        <FontAwesomeIcon icon={faHeart} />
+                    </button>
+                </div>
+                <div className="login">
+                    <button
+                        type="button"
+                        onClick={() => console.log("User clicked")}
+                    >
+                        <FontAwesomeIcon icon={faUser} />
+                    </button>
+                </div>
+            </div>
+        </header>
+    );
 };
 
 export default Navbar;
