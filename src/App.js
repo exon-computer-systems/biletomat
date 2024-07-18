5f694ca9ba56e34720b5cc0b09f3863b6c5e8a29
Szymon <admin@example.com>
2024-07-18 11:00:12 +0200

    Init router structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import "./App.css";
-import Navbar from "./pages/components/Navbar";
+
 function App() {
     return (
-        <div className="App">
-            <Navbar />
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+const Home = () => {
+    return (
+        <>
+            <Navbar />
+            <section className="home-wrap">home</section>
+        </>
+    );
+};
+
+export default Home;
