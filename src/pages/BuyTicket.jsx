a24ad01328e382c7c35c52008e7eb457383c9955
Patryk Kulpa <patkul004@pbs.edu.pl>
2024-07-18 18:16:16 +0200

    a

--- src/pages/BuyTicket.jsx
+++ src/pages/BuyTicket.jsx
@@ -1,13 +1,41 @@
 import React, { useState } from "react";
-import TicketBox from "./components/TicketBox";
+
 import "./BuyTicket.css";
-import SumTicketPrice from "./components/SumTicketPrice";
+
+const dataTicket = [
+  {
+    title: "Dorośli",
+    price: 100,
+    name: "adults",
+  },
+  {
+    title: "Dzieci",
+    price: 50,
+    name: "children",
+  },
+  {
+    title: "Rodzinny 2+2",
+    price: 280,
+    name: "family",
+  },
+  {
+    title: "VIP",
+    price: 300,
+    name: "vip",
+  },
+];
+
 const BuyTicket = () => {
-  const [value, setValue] = useState("");
   const [summary, setSummary] = useState("0 PLN");
+  const [order, setOrder] = useState({
+    adults: 0,
+    children: 0,
+    family: 0,
+    vip: 0,
+  });
 
   const onChange = e => {
-    setValue(e.target.value);
+    setOrder(e.target.value);
   };
 
   return (
@@ -19,17 +47,64 @@ const BuyTicket = () => {
         </div>
 
         <section className="choice-boxes">
-          <TicketBox
-            value={value}
-            setValue={setValue}
-            onChange={onChange}
-            summary={summary}
-            setSummary={setSummary}
-          />
+          <div className="choice-box">
+            <h2>{dataTicket[0].title}</h2>
+            <div>
+              <span>{dataTicket[0].price} PLN</span>
+              <input
+                name={dataTicket.name}
+                type="number"
+                value={order}
+                onChange={onChange}
+              />
+            </div>
+          </div>
+
+          <div className="choice-box">
+            <h2>{dataTicket[1].title}</h2>
+            <div>
+              <span>{dataTicket[1].price} PLN</span>
+              <input
+                name={dataTicket.name}
+                type="number"
+                value={order}
+                onChange={onChange}
+              />
+            </div>
+          </div>
+
+          <div className="choice-box">
+            <h2>{dataTicket[2].title}</h2>
+            <div>
+              <span>{dataTicket[2].price} PLN</span>
+              <input
+                name={dataTicket.name}
+                type="number"
+                value={order}
+                onChange={onChange}
+              />
+            </div>
+          </div>
+
+          <div className="choice-box">
+            <h2>{dataTicket[3].title}</h2>
+            <div>
+              <span>{dataTicket[3].price} PLN</span>
+              <input
+                name={dataTicket.name}
+                type="number"
+                value={order}
+                onChange={onChange}
+              />
+            </div>
+          </div>
         </section>
 
         <div className="summary">
-          <SumTicketPrice />
+          <h2>
+            Całość - <span>{summary}</span>
+          </h2>
+          <button>REZERWUJ</button>
         </div>
       </section>
     </div>
