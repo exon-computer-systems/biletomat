679fde111f7f78df4d5a112d6474bf98520eece8
Szymon <admin@example.com>
2024-07-24 09:42:53 +0200

    Fiexed feching data

--- src/pages/components/eventList/EventList.jsx
+++ src/pages/components/eventList/EventList.jsx
@@ -3,31 +3,31 @@ import "./EventList.css";
 import Event from "./Event";
 
 const EventList = ({ events, isLoading }) => {
-  return (
-    <section className="e-list-wrap">
-      <section className="e-list">
-        <h2 className="e-list-title">Najblizsze wydarzenia</h2>
-        <section className="e-list-events">
-          {isLoading ? (
-            <p>Loading events...</p>
-          ) : (
-            events.map((el, idx) => (
-              <Event
-                key={idx}
-                title={el.attributes.title}
-                desc={el.attributes.description}
-                date={el.attributes.startDate}
-                city={el.attributes.city}
-                coverImage={el.attributes.coverLink}
-                sale={el.attributes.sale}
-                goingFast={el.attributes.goingFast}
-              />
-            ))
-          )}
+    return (
+        <section className="e-list-wrap">
+            <section className="e-list">
+                <h2 className="e-list-title">Najblizsze wydarzenia</h2>
+                <section className="e-list-events">
+                    {isLoading ? (
+                        <p>Loading events...</p>
+                    ) : (
+                        events.map((el, idx) => (
+                            <Event
+                                key={el.attributes.uid}
+                                title={el.attributes.title}
+                                desc={el.attributes.description}
+                                date={el.attributes.startDate}
+                                city={el.attributes.city}
+                                coverImage={el.attributes.coverLink}
+                                sale={el.attributes.sale}
+                                goingFast={el.attributes.goingFast}
+                            />
+                        ))
+                    )}
+                </section>
+            </section>
         </section>
-      </section>
-    </section>
-  );
+    );
 };
 
 export default EventList;
--- src/pages/components/slider/Slider.jsx
+++ src/pages/components/slider/Slider.jsx
@@ -5,30 +5,30 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
 const Slider = ({ events, isLoading }) => {
-  const [width, setWidth] = useState(400);
+    const [width, setWidth] = useState(300);
 
-  const ref = useRef(0);
+    const ref = useRef(0);
 
-  const scrollBy = scrollOffset => {
-    ref.current.scrollLeft += scrollOffset;
-  };
+    const scrollBy = (scrollOffset) => {
+        ref.current.scrollLeft += scrollOffset;
+    };
 
-  return (
-    <section className="slider-wrap">
-      <button
-        className="slider-btn slider-btn-left"
-        onClick={() => scrollBy(-width)}
-      >
-        <FontAwesomeIcon icon={faAngleLeft} />
-      </button>
-      <button
-        className="slider-btn slider-btn-right"
-        onClick={() => scrollBy(width)}
-      >
-        <FontAwesomeIcon icon={faAngleRight} />
-      </button>
-      <section className="slider" ref={ref}>
-        {/* {data
+    return (
+        <section className="slider-wrap">
+            <button
+                className="slider-btn slider-btn-left"
+                onClick={() => scrollBy(-width)}
+            >
+                <FontAwesomeIcon icon={faAngleLeft} />
+            </button>
+            <button
+                className="slider-btn slider-btn-right"
+                onClick={() => scrollBy(width)}
+            >
+                <FontAwesomeIcon icon={faAngleRight} />
+            </button>
+            <section className="slider" ref={ref}>
+                {/* {data
                     // ?.filter((el, idx) => idx < 5)
                     .map((el, idx) => {
                         return (
@@ -43,23 +43,23 @@ const Slider = ({ events, isLoading }) => {
                         );
                     })} */}
 
-        {isLoading ? (
-          <p>Loading events...</p>
-        ) : (
-          events.map(el => (
-            <Card
-              key={el.attributes.uid}
-              title={el.attributes.title}
-              date={el.attributes.startDate}
-              city={el.attributes.city}
-              coverImage={el.attributes.coverLink}
-              width={width}
-            />
-          ))
-        )}
-      </section>
-    </section>
-  );
+                {isLoading ? (
+                    <p>Loading events...</p>
+                ) : (
+                    events.map((el) => (
+                        <Card
+                            key={el.attributes.uid}
+                            title={el.attributes.title}
+                            date={el.attributes.startDate}
+                            city={el.attributes.city}
+                            coverImage={el.attributes.coverLink}
+                            width={width}
+                        />
+                    ))
+                )}
+            </section>
+        </section>
+    );
 };
 
 export default Slider;
