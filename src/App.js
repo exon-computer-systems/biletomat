25ddb5942f2f515a8b0f931b5e5a991ede610637
Patryk Kulpa <patkul004@pbs.edu.pl>
2024-07-30 23:45:30 +0200

    added edit post page

--- src/App.js
+++ src/App.js
@@ -7,35 +7,33 @@ import LogIn from "./pages/components/logIn/LogIn";
 import EventPage from "./pages/eventPage/EventPage";
 import RequireAuth from "./pages/components/RequireAuth";
 import UserPage from "./pages/components/userPage/UserPage";
+import EditPage from "./pages/components/editPage/EditPage";
 
 function App() {
-    const ROLES = {
-        User: 2001,
-        Editor: 1984,
-        Admin: 5150,
-    };
+  const ROLES = {
+    User: 2001,
+    Editor: 1984,
+    Admin: 5150,
+  };
 
-    return (
-        <BrowserRouter>
-            <Routes>
-                {/* public routes */}
-                <Route path="/" element={<Home />} />
-                <Route path={`/event/:id`} element={<EventPage />} />
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* public routes */}
+        <Route path="/" element={<Home />} />
+        <Route path={`/event/:id`} element={<EventPage />} />
 
-                {/* protected routes */}
-                <Route
-                    element={
-                        <RequireAuth
-                            allowedRoles={[ROLES.Editor, ROLES.Admin]}
-                        />
-                    }
-                >
-                    <Route path="/user" element={<UserPage />} />
-                    <Route path="/buy" element={<BuyTicket />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );
+        {/* protected routes */}
+        <Route
+          element={<RequireAuth allowedRoles={[ROLES.Editor, ROLES.Admin]} />}
+        >
+          <Route path="/user" element={<UserPage />} />
+          <Route path="/buy" element={<BuyTicket />} />
+        </Route>
+        <Route path="/edit-page" element={<EditPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
