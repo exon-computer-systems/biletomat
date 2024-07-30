42d733f1510fa02aff566fd42f049dd2c86a176e
Szymon <szmn.rtn@gmail.com>
2024-07-30 21:32:11 +0200

    Added protecting route to userpage

--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -5,44 +5,50 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useNavigate } from "react-router";
 
 const Navbar = ({ setIsLogged, isLogged, close, open }) => {
-  //   const [isClicked, setIsClicked] = useState(false);
+    //   const [isClicked, setIsClicked] = useState(false);
 
-  const nav = useNavigate();
+    const nav = useNavigate();
 
-  return (
-    <>
-      <div className="wave-background"></div>
-      <header className="navbar">
-        <div className="logo" onClick={() => nav("/")}>
-          <img
-            className="logoImage"
-            src="https://motocms.exon.pl/mt-content/uploads/2021/08/exon-logo-biel.png"
-            alt="exon logo"
-          />
-        </div>
-        <div className="icons">
-          <div className="favorites">
-            <button
-              className="nav-btn fav-btn"
-              type="button"
-              onClick={() => console.log("Favourite clicked")}
-            >
-              <FontAwesomeIcon icon={faHeart} className="nav-icn" />
-            </button>
-          </div>
-          <div className="login">
-            <button
-              className="nav-btn log-btn"
-              type="button"
-              onClick={() => (isLogged ? close() : open())}
-            >
-              <FontAwesomeIcon icon={faUser} className="nav-icn" />
-            </button>
-          </div>
-        </div>
-      </header>
-    </>
-  );
+    return (
+        <>
+            <div className="wave-background"></div>
+            <header className="navbar">
+                <div className="logo" onClick={() => nav("/")}>
+                    <img
+                        className="logoImage"
+                        src="https://motocms.exon.pl/mt-content/uploads/2021/08/exon-logo-biel.png"
+                        alt="exon logo"
+                    />
+                </div>
+                <div className="icons">
+                    <div className="favorites">
+                        <button
+                            className="nav-btn fav-btn"
+                            type="button"
+                            onClick={() => nav("/user")}
+                        >
+                            <FontAwesomeIcon
+                                icon={faHeart}
+                                className="nav-icn"
+                            />
+                        </button>
+                    </div>
+                    <div className="login">
+                        <button
+                            className="nav-btn log-btn"
+                            type="button"
+                            onClick={() => (isLogged ? close() : open())}
+                        >
+                            <FontAwesomeIcon
+                                icon={faUser}
+                                className="nav-icn"
+                            />
+                        </button>
+                    </div>
+                </div>
+            </header>
+        </>
+    );
 };
 
 export default Navbar;
--- src/pages/components/RequireAuth.jsx
+++ src/pages/components/RequireAuth.jsx
@@ -5,7 +5,7 @@ const RequireAuth = ({ allowedRoles }) => {
     const { auth } = useAuth();
     const location = useLocation();
 
-    return auth?.roles.find((role) => allowedRoles.includes(role)) ? (
+    return auth?.roles?.find((role) => allowedRoles.includes(role)) ? (
         <Outlet />
     ) : (
         <Navigate to="/" state={{ from: location }} replace />
