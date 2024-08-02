1e1a33cc8202d9c5eb2d35ec391bfab0abe0aa72
Szymon <szmn.rtn@gmail.com>
2024-08-02 09:28:21 +0200

    Some changes

--- src/App.js
+++ src/App.js
@@ -7,35 +7,39 @@ import LogIn from "./pages/components/logIn/LogIn";
 import EventPage from "./pages/eventPage/EventPage";
 import RequireAuth from "./pages/components/RequireAuth";
 import UserPage from "./pages/components/userPage/UserPage";
-import EditPage from "./pages/components/editPage/EditPage";
+import EditPage from "./pages/editPage/EditPage";
 
 function App() {
-  const ROLES = {
-    User: 2001,
-    Editor: 1984,
-    Admin: 5150,
-  };
+    const ROLES = {
+        User: 2001,
+        Editor: 1984,
+        Admin: 5150,
+    };
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* public routes */}
-        <Route path="/" element={<Home />} />
-        <Route path={`/event/:id`} element={<EventPage />} />
+    return (
+        <BrowserRouter>
+            <Routes>
+                {/* public routes */}
+                <Route path="/" element={<Home />} />
+                <Route path={`/event/:id`} element={<EventPage />} />
 
-        {/* protected routes */}
-        <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
-          <Route path="/user" element={<UserPage />} />
-        </Route>
-        <Route
-          element={<RequireAuth allowedRoles={[ROLES.Editor, ROLES.Admin]} />}
-        >
-          <Route path="/buy" element={<BuyTicket />} />
-          <Route path="/edit-page" element={<EditPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+                {/* protected routes */}
+                <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
+                    <Route path="/user" element={<UserPage />} />
+                </Route>
+                <Route
+                    element={
+                        <RequireAuth
+                            allowedRoles={[ROLES.Editor, ROLES.Admin]}
+                        />
+                    }
+                >
+                    <Route path="/buy" element={<BuyTicket />} />
+                    <Route path="/edit-page" element={<EditPage />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
