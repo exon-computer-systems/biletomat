6e6fafe9bccec40e7d3cee99006ebfbd5782c10a
Patryk Kulpa <patkul004@pbs.edu.pl>
2024-08-04 23:02:35 +0200

    fixed auth

--- src/pages/components/authPanel/AuthPanel.jsx
+++ src/pages/components/authPanel/AuthPanel.jsx
@@ -1,36 +1,48 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./AuthPanel.css";
 import LoginForm from "./LoginForm";
 import RegisterForm from "./RegisterForm";
+import CheckmarkAnimation from "./CheckmarkAnimation"; // Make sure to import this if it's in a separate file
 
 const AuthPanel = ({ handleClose }) => {
-    const [showLogin, setShowLogin] = useState(true);
+  const [showLogin, setShowLogin] = useState(true);
+  const [response, setResponse] = useState(false);
 
-    return (
-        <section className="overlay" onClick={handleClose}>
-            <section
-                className="apanel-cont"
-                onClick={(e) => e.stopPropagation()}
-            >
-                <section className="apanel-switch-cont">
-                    <button
-                        className="apanel-switch-btn active"
-                        onClick={() => setShowLogin(true)}
-                    >
-                        Login
-                    </button>
-                    <span className="vert-line"></span>
-                    <button
-                        className="apanel-switch-btn"
-                        onClick={() => setShowLogin(false)}
-                    >
-                        Sign up
-                    </button>
-                </section>
-                {showLogin ? <LoginForm /> : <RegisterForm />}
+  return (
+    <section className="overlay" onClick={handleClose}>
+      <section className="apanel-cont" onClick={e => e.stopPropagation()}>
+        {response ? (
+          <CheckmarkAnimation />
+        ) : (
+          <>
+            <section className="apanel-switch-cont">
+              <button
+                className={`apanel-switch-btn ${showLogin ? "active" : ""}`}
+                onClick={() => setShowLogin(true)}
+              >
+                Login
+              </button>
+              <span className="vert-line"></span>
+              <button
+                className={`apanel-switch-btn ${!showLogin ? "active" : ""}`}
+                onClick={() => setShowLogin(false)}
+              >
+                Sign up
+              </button>
             </section>
-        </section>
-    );
+            {showLogin ? (
+              <LoginForm handleClose={handleClose} setResponse={setResponse} />
+            ) : (
+              <RegisterForm
+                handleClose={handleClose}
+                setResponse={setResponse}
+              />
+            )}
+          </>
+        )}
+      </section>
+    </section>
+  );
 };
 
 export default AuthPanel;
--- src/pages/components/authPanel/RegisterForm.jsx
+++ src/pages/components/authPanel/RegisterForm.jsx
@@ -3,241 +3,239 @@ import axios from "../../api/axios";
 // import axios from "axios";
 import useAuth from "../../hooks/useAuth";
 
-const RegisterForm = () => {
-    const { setAuth, auth } = useAuth();
+const RegisterForm = ({ handleClose, setResponse }) => {
+  const { setAuth, auth } = useAuth();
 
-    const emailRef = useRef();
+  const emailRef = useRef();
 
-    const [nextStep, setNextStep] = useState(false);
-    const [userData, setUserData] = useState({
-        firstName: "",
-        lastName: "",
-        email: "",
-        password: "",
-        passwordConfirmation: "",
-        age: 22,
-    });
-    const [errorMsg, setErrorMsg] = useState("");
-    const [matchPwd, setMatchPwd] = useState(false);
+  const [nextStep, setNextStep] = useState(false);
+  const [userData, setUserData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    passwordConfirmation: "",
+    age: 22,
+  });
+  const [errorMsg, setErrorMsg] = useState("");
+  const [matchPwd, setMatchPwd] = useState(false);
 
-    const handleNextStep = (e) => {
-        e.preventDefault();
-        setNextStep(true);
-    };
+  const handleNextStep = e => {
+    e.preventDefault();
+    setNextStep(true);
+  };
 
-    useEffect(() => {
-        emailRef.current?.focus();
-    }, []);
+  useEffect(() => {
+    emailRef.current?.focus();
+  }, []);
 
-    useEffect(() => {
-        const { password, passwordConfirmation } = userData;
-        setMatchPwd(password === passwordConfirmation);
-        setErrorMsg(
-            password === passwordConfirmation ? "" : "Passwords dont match"
-        );
-    }, [userData.password, userData.passwordConfirmation]);
-
-    // useEffect(() => {
-
-    // }, [birthDate]);
-
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setUserData((prev) => ({ ...prev, [name]: value }));
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        // register
-        try {
-            const response = await axios.post(
-                "/register",
-                JSON.stringify({
-                    firstName: userData.firstName,
-                    lastName: userData.lastName,
-                    email: userData.email,
-                    password: userData.password,
-                    age: userData.age,
-                }),
-                {
-                    headers: { "Content-Type": "application/json" },
-                    withCredentials: true,
-                }
-            );
-            console.log(JSON.stringify(response?.data.success));
-
-            // login after success register
-            if (response.data.success) {
-                try {
-                    const response = await axios.post(
-                        "/auth",
-                        // "https://biletomat-be.onrender.com/auth",
-                        // "http://localhost:3500/auth",
-                        JSON.stringify({
-                            email: userData.email,
-                            password: userData.password,
-                        }),
-                        {
-                            headers: { "Content-Type": "application/json" },
-                            withCredentials: true,
-                        }
-                    );
-                    console.log(JSON.stringify(response?.data));
-                    //console.log(JSON.stringify(response));
-                    const accessToken = response?.data?.accessToken;
-                    const roles = response?.data?.roles;
-
-                    setAuth({
-                        email: userData.email,
-                        password: userData.password,
-                        roles,
-                        accessToken,
-                    });
-                } catch (err) {
-                    if (!err?.response) {
-                        console.error("No server response");
-                    } else if (err.response?.status == 400) {
-                        console.error("Missing username or password");
-                    } else if (err.response?.status == 401) {
-                        console.error("Unauthorized");
-                    } else {
-                        console.error("Login failed");
-                    }
-                }
-            }
-        } catch (err) {
-            console.error(err);
-        }
-    };
-
-    return (
-        <section className="apanel-form-cont register-form-cont">
-            <h1 className="apanel-title register-title">Utwórz konto</h1>
-            <form className="register-form apanel-form" onSubmit={handleSubmit}>
-                <section className="apanel-inputs">
-                    {nextStep ? (
-                        <>
-                            <label className="register-label apanel-label">
-                                <p className="register-label-text">
-                                    First name
-                                </p>
-                                <input
-                                    className="apanel-input"
-                                    type="text"
-                                    name="firstName"
-                                    onChange={handleInputChange}
-                                    value={userData.firstName}
-                                    required
-                                />
-                            </label>
-                            <label className="register-label apanel-label">
-                                <p className="register-label-text">Last name</p>
-                                <input
-                                    className="apanel-input"
-                                    type="text"
-                                    name="lastName"
-                                    onChange={handleInputChange}
-                                    value={userData.lastName}
-                                    required
-                                />
-                            </label>
-
-                            <label className="register-label apanel-label">
-                                <p className="register-label-text">
-                                    Date of birth
-                                </p>
-                                <input
-                                    className="apanel-input"
-                                    type="number"
-                                    name="age"
-                                    onChange={handleInputChange}
-                                    value={userData.age}
-                                    required
-                                />
-                            </label>
-                        </>
-                    ) : (
-                        <>
-                            <label className="register-label apanel-label">
-                                <p className="register-label-text">Email</p>
-                                <input
-                                    className="apanel-input"
-                                    type="email"
-                                    name="email"
-                                    ref={emailRef}
-                                    onChange={handleInputChange}
-                                    value={userData.email}
-                                    required
-                                />
-                            </label>
-                            <label className="register-label apanel-label">
-                                <p className="register-label-text">Password</p>
-                                <input
-                                    className="apanel-input"
-                                    type="password"
-                                    name="password"
-                                    onChange={handleInputChange}
-                                    value={userData.password}
-                                    required
-                                />
-                            </label>
-                            <label className="register-label apanel-label">
-                                <p className="register-label-text">
-                                    Confirm password
-                                </p>
-                                <input
-                                    className="apanel-input"
-                                    type="password"
-                                    name="passwordConfirmation"
-                                    onChange={handleInputChange}
-                                    value={userData.passwordConfirmation}
-                                    required
-                                />
-                            </label>
-                        </>
-                    )}
-                    <p className="apanel-error">{errorMsg}</p>
-                </section>
-                <section className="apanel-btn-cont">
-                    {nextStep ? (
-                        <>
-                            <button
-                                type="button"
-                                className="register-btn apanel-btn"
-                                onClick={() => setNextStep(false)}
-                            >
-                                Go back
-                            </button>
-                            <button
-                                type="submit"
-                                className="register-btn apanel-btn"
-                                onSubmit={handleSubmit}
-                            >
-                                Register
-                            </button>
-                        </>
-                    ) : (
-                        <button
-                            disabled={
-                                !userData.email ||
-                                !userData.password ||
-                                !userData.passwordConfirmation ||
-                                !matchPwd
-                                    ? true
-                                    : false
-                            }
-                            type="button"
-                            className="register-btn apanel-btn"
-                            onClick={handleNextStep}
-                        >
-                            Next step
-                        </button>
-                    )}
-                </section>
-            </form>
-        </section>
+  useEffect(() => {
+    const { password, passwordConfirmation } = userData;
+    setMatchPwd(password === passwordConfirmation);
+    setErrorMsg(
+      password === passwordConfirmation ? "" : "Passwords dont match"
     );
+  }, [userData.password, userData.passwordConfirmation]);
+
+  // useEffect(() => {
+
+  // }, [birthDate]);
+
+  const handleInputChange = e => {
+    const { name, value } = e.target;
+    setUserData(prev => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    // register
+    try {
+      const response = await axios.post(
+        "/register",
+        JSON.stringify({
+          firstName: userData.firstName,
+          lastName: userData.lastName,
+          email: userData.email,
+          password: userData.password,
+          age: userData.age,
+        }),
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      );
+      console.log(JSON.stringify(response?.data.success));
+
+      // login after success register
+      if (response.data.success) {
+        try {
+          const response = await axios.post(
+            "/auth",
+            // "https://biletomat-be.onrender.com/auth",
+            // "http://localhost:3500/auth",
+            JSON.stringify({
+              email: userData.email,
+              password: userData.password,
+            }),
+            {
+              headers: { "Content-Type": "application/json" },
+              withCredentials: true,
+            }
+          );
+          console.log(JSON.stringify(response?.data));
+          //console.log(JSON.stringify(response));
+          setResponse(true);
+          const accessToken = response?.data?.accessToken;
+          const roles = response?.data?.roles;
+
+          setAuth({
+            email: userData.email,
+            password: userData.password,
+            roles,
+            accessToken,
+          });
+          setTimeout(() => {
+            handleClose();
+          }, 2000);
+        } catch (err) {
+          if (!err?.response) {
+            console.error("No server response");
+          } else if (err.response?.status == 400) {
+            console.error("Missing username or password");
+          } else if (err.response?.status == 401) {
+            console.error("Unauthorized");
+          } else {
+            console.error("Login failed");
+          }
+        }
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <section className="apanel-form-cont register-form-cont">
+      <h1 className="apanel-title register-title">Utwórz konto</h1>
+      <form className="register-form apanel-form" onSubmit={handleSubmit}>
+        <section className="apanel-inputs">
+          {nextStep ? (
+            <>
+              <label className="register-label apanel-label">
+                <p className="register-label-text">First name</p>
+                <input
+                  className="apanel-input"
+                  type="text"
+                  name="firstName"
+                  onChange={handleInputChange}
+                  value={userData.firstName}
+                  required
+                />
+              </label>
+              <label className="register-label apanel-label">
+                <p className="register-label-text">Last name</p>
+                <input
+                  className="apanel-input"
+                  type="text"
+                  name="lastName"
+                  onChange={handleInputChange}
+                  value={userData.lastName}
+                  required
+                />
+              </label>
+
+              <label className="register-label apanel-label">
+                <p className="register-label-text">Date of birth</p>
+                <input
+                  className="apanel-input"
+                  type="number"
+                  name="age"
+                  onChange={handleInputChange}
+                  value={userData.age}
+                  required
+                />
+              </label>
+            </>
+          ) : (
+            <>
+              <label className="register-label apanel-label">
+                <p className="register-label-text">Email</p>
+                <input
+                  className="apanel-input"
+                  type="email"
+                  name="email"
+                  ref={emailRef}
+                  onChange={handleInputChange}
+                  value={userData.email}
+                  required
+                />
+              </label>
+              <label className="register-label apanel-label">
+                <p className="register-label-text">Password</p>
+                <input
+                  className="apanel-input"
+                  type="password"
+                  name="password"
+                  onChange={handleInputChange}
+                  value={userData.password}
+                  required
+                />
+              </label>
+              <label className="register-label apanel-label">
+                <p className="register-label-text">Confirm password</p>
+                <input
+                  className="apanel-input"
+                  type="password"
+                  name="passwordConfirmation"
+                  onChange={handleInputChange}
+                  value={userData.passwordConfirmation}
+                  required
+                />
+              </label>
+            </>
+          )}
+          <p className="apanel-error">{errorMsg}</p>
+        </section>
+        <section className="apanel-btn-cont">
+          {nextStep ? (
+            <>
+              <button
+                type="button"
+                className="register-btn apanel-btn"
+                onClick={() => setNextStep(false)}
+              >
+                Go back
+              </button>
+              <button
+                type="submit"
+                className="register-btn apanel-btn"
+                onSubmit={handleSubmit}
+              >
+                Register
+              </button>
+            </>
+          ) : (
+            <button
+              disabled={
+                !userData.email ||
+                !userData.password ||
+                !userData.passwordConfirmation ||
+                !matchPwd
+                  ? true
+                  : false
+              }
+              type="button"
+              className="register-btn apanel-btn"
+              onClick={handleNextStep}
+            >
+              Next step
+            </button>
+          )}
+        </section>
+      </form>
+    </section>
+  );
 };
 
 export default RegisterForm;
