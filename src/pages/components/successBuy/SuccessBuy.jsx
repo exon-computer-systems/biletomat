4f1452b79ca8ffa94a8258b5ea1e757690c636c5
Patryk Kulpa <patkul004@pbs.edu.pl>
2024-08-13 11:59:48 +0200

    change

--- src/pages/components/successBuy/SuccessBuy.jsx
+++ src/pages/components/successBuy/SuccessBuy.jsx
@@ -3,41 +3,52 @@ import "./SuccessBuy.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheckCircle } from "@fortawesome/free-regular-svg-icons";
 import { useNavigate } from "react-router-dom";
+import axios from "../../api/axios";
 
 const SuccessBuy = () => {
-    const nav = useNavigate();
+  const nav = useNavigate();
 
-    return (
-        <section className="succbuy-cont">
-            <section className="succbuy">
-                <h1 className="succbuy-title">Pomyślnie zakupiono bilet</h1>
-                <FontAwesomeIcon
-                    className="succbuy-icon"
-                    icon={faCheckCircle}
-                />
-            </section>
-            <section className="succbuy-btn-cont">
-                <p className="succbuy-text">
-                    Bilet dostepny w panelu użytkownika
-                </p>
+  const fetchData = async () => {
+    try {
+      const response = await axios.post(
+        "/client-print",
+        { data: "Your print data here" },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log("Print request sent successfully");
+    } catch (error) {
+      console.error("Error sending print request:", error);
+    }
+    console.log("printing");
+  };
 
-                <section className="succbuy-btn-wrap">
-                    <button
-                        onClick={() => console.log("print qr code")}
-                        className="succbuy-btn nonprimary"
-                    >
-                        Drukuj
-                    </button>
-                    <button
-                        onClick={() => nav("/")}
-                        className="succbuy-btn primary"
-                    >
-                        Pomiń
-                    </button>
-                </section>
-            </section>
+  return (
+    <section className="succbuy-cont">
+      <section className="succbuy">
+        <h1 className="succbuy-title">Pomyślnie zakupiono bilet</h1>
+        <FontAwesomeIcon className="succbuy-icon" icon={faCheckCircle} />
+      </section>
+      <section className="succbuy-btn-cont">
+        <p className="succbuy-text">Bilet dostepny w panelu użytkownika</p>
+
+        <section className="succbuy-btn-wrap">
+          <button
+            onClick={() => fetchData()}
+            className="succbuy-btn nonprimary"
+          >
+            Drukuj
+          </button>
+          <button onClick={() => nav("/")} className="succbuy-btn primary">
+            Pomiń
+          </button>
         </section>
-    );
+      </section>
+    </section>
+  );
 };
 
 export default SuccessBuy;
