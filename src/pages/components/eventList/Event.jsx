465f54693771147f43077ba5e19f77ca9cf595c3
Patryk Kulpa <patkul004@pbs.edu.pl>
2024-08-29 11:32:05 +0200

    change

--- src/pages/components/eventList/Event.jsx
+++ src/pages/components/eventList/Event.jsx
@@ -5,48 +5,48 @@ import { faHeart as emptyHeart } from "@fortawesome/free-regular-svg-icons";
 import { useNavigate } from "react-router-dom";
 
 const Event = ({
-  id,
-  tid,
-  title,
-  date,
-  city,
-  coverImage,
-  sale,
-  goingFast,
-  isFavorite,
-  toggleFavorite,
+    id,
+    tid,
+    title,
+    date,
+    city,
+    coverImage,
+    sale,
+    goingFast,
+    isFavorite,
+    toggleFavorite,
 }) => {
-  const nav = useNavigate();
+    const nav = useNavigate();
 
-  return (
-    <section
-      className="search-results-wrapper event-wrap"
-      // onClick={() => nav(`/event/${tid}`)}
-    >
-      <div className="result-img-cover">
-        <img src={coverImage} alt={`${title} cover`} />
-      </div>
-      <section className="results-info">
-        <div>
-          <div className="title-favourite">
-            <h2>{title}</h2>
-            <FontAwesomeIcon
-              icon={isFavorite ? fullHeart : emptyHeart}
-              className="heart-icon"
-              onClick={e => {
-                e.stopPropagation(); // Prevent navigation on icon click
-                toggleFavorite();
-              }}
-            />
-          </div>
-          <p>
-            {date} | {city}
-          </p>
-        </div>
-        <button onClick={() => nav(`/event/${tid}`)}>Kup bilet</button>
-      </section>
-    </section>
-  );
+    return (
+        <section
+            className="search-results-wrapper event-wrap"
+            // onClick={() => nav(`/event/${tid}`)}
+        >
+            <div className="result-img-cover">
+                <img src={coverImage} alt={`${title} cover`} />
+            </div>
+            <section className="results-info">
+                <div>
+                    <div className="title-favourite">
+                        <h2>{title}</h2>
+                        <FontAwesomeIcon
+                            icon={isFavorite ? fullHeart : emptyHeart}
+                            className="heart-icon"
+                            onClick={e => {
+                                e.stopPropagation(); // Prevent navigation on icon click
+                                toggleFavorite();
+                            }}
+                        />
+                    </div>
+                    <p>
+                        {date} | {city}
+                    </p>
+                </div>
+                <button onClick={() => nav(`/event/${tid}`)}>Kup bilet</button>
+            </section>
+        </section>
+    );
 };
 
 export default Event;
--- src/pages/components/eventList/EventList.jsx
+++ src/pages/components/eventList/EventList.jsx
@@ -4,53 +4,53 @@ import Event from "./Event";
 import useAuth from "../../hooks/useAuth";
 
 const EventList = ({ events, isLoading }) => {
-  const { auth } = useAuth();
-  const [favorites, setFavorites] = useState([]);
+    const { auth } = useAuth();
+    const [favorites, setFavorites] = useState([]);
 
-  useEffect(() => {
-    // Initialize favorite states for each event
-    const initialFavorites = events.map(event =>
-      auth?.likedEvents?.includes(event.tid)
-    );
-    setFavorites(initialFavorites);
-  }, [auth, events]);
+    useEffect(() => {
+        // Initialize favorite states for each event
+        const initialFavorites = events.map(event =>
+            auth?.likedEvents?.includes(event.tid)
+        );
+        setFavorites(initialFavorites);
+    }, [auth, events]);
 
-  const toggleFavorite = index => {
-    const updatedFavorites = [...favorites];
-    updatedFavorites[index] = !updatedFavorites[index];
-    setFavorites(updatedFavorites);
+    const toggleFavorite = index => {
+        const updatedFavorites = [...favorites];
+        updatedFavorites[index] = !updatedFavorites[index];
+        setFavorites(updatedFavorites);
 
-    // Here you could also update the user's favorite events on the backend or in local storage
-  };
+        // Here you could also update the user's favorite events on the backend or in local storage
+    };
 
-  return (
-    <section className="e-list-wrap">
-      <section className="e-list">
-        <h2 className="e-list-title">Najblizsze wydarzenia</h2>
-        <section className="e-list-events">
-          {isLoading ? (
-            <p>Loading events...</p>
-          ) : (
-            events.map((el, idx) => (
-              <Event
-                id={el.id}
-                tid={el.tid}
-                key={idx}
-                title={el.title}
-                date={el.startDate}
-                city={el.city}
-                coverImage={el.coverImage}
-                sale={el.sale}
-                goingFast={el.goingFast}
-                isFavorite={favorites[idx]}
-                toggleFavorite={() => toggleFavorite(idx)}
-              />
-            ))
-          )}
+    return (
+        <section className="e-list-wrap">
+            <section className="e-list">
+                <h2 className="e-list-title">Najblizsze wydarzenia</h2>
+                <section className="e-list-events">
+                    {isLoading ? (
+                        <p>Loading events...</p>
+                    ) : (
+                        events.map((el, idx) => (
+                            <Event
+                                id={el.id}
+                                tid={el.tid}
+                                key={idx}
+                                title={el.title}
+                                date={el.startDate}
+                                city={el.city}
+                                coverImage={el.coverImage}
+                                sale={el.sale}
+                                goingFast={el.goingFast}
+                                isFavorite={favorites[idx]}
+                                toggleFavorite={() => toggleFavorite(idx)}
+                            />
+                        ))
+                    )}
+                </section>
+            </section>
         </section>
-      </section>
-    </section>
-  );
+    );
 };
 
 export default EventList;
